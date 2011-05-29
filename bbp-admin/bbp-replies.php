myfossil/bbPress

commit 72e4f645b9a861f96682213f3a85aa707302533a

Improve support for orphaned topics and replies in admin post rows by displaying "No Forum" and "No Topic" if parents have been deleted.
git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3260 9866e705-20ec-0310-96e7-cbb4277adcfb

bbp-admin/bbp-replies.php
@@ -568,24 +568,33 @@ class BBP_Replies_Admin {
 
 				// Output forum name
 				if ( !empty( $topic_id ) ) {
-					bbp_topic_title( $topic_id );
 
-					// Link information
-					$actions = apply_filters( 'reply_topic_row_actions', array (
-						'edit' => '<a href="' . add_query_arg( array( 'post' => $topic_id, 'action' => 'edit' ), admin_url( '/post.php' ) ) . '">' . __( 'Edit', 'bbpress' ) . '</a>',
-						'view' => '<a href="' . bbp_get_topic_permalink( $topic_id ) . '">' . __( 'View', 'bbpress' ) . '</a>'
-					) );
+					// Topic Title
+					if ( !$topic_title = bbp_topic_title( $topic_id ) )
+						$topic_title = __( 'No Topic', 'bbpress' );
 
-					// Output forum post row links
-					foreach ( $actions as $action => $link )
-						$formatted_actions[] = '<span class="' . $action . '">' . $link . '</span>';
+					// Output the title
+					echo $topic_title;
 
-					if ( !empty( $topic_id ) )
+					// Show actions if topic exists
+					if ( $topic_title != __( 'No Topic', 'bbpress' ) ) {
+
+						// Link information
+						$actions = apply_filters( 'reply_topic_row_actions', array (
+							'edit' => '<a href="' . add_query_arg( array( 'post' => $topic_id, 'action' => 'edit' ), admin_url( '/post.php' ) ) . '">' . __( 'Edit', 'bbpress' ) . '</a>',
+							'view' => '<a href="' . bbp_get_topic_permalink( $topic_id ) . '">' . __( 'View', 'bbpress' ) . '</a>'
+						) );
+
+						// Output forum post row links
+						foreach ( $actions as $action => $link )
+							$formatted_actions[] = '<span class="' . $action . '">' . $link . '</span>';
+
 						echo '<div class="row-actions">' . implode( ' | ', $formatted_actions ) . '</div>';
+					}
 
 				// Reply has no topic
 				} else {
-					_e( '(No Topic)', 'bbpress' );
+					_e( 'No Topic', 'bbpress' );
 				}
 
 				break;
@@ -598,25 +607,34 @@ class BBP_Replies_Admin {
 
 				// Output forum name
 				if ( !empty( $forum_id ) ) {
-					bbp_forum_title( $forum_id );
 
-					// Link information
-					$actions = apply_filters( 'reply_topic_forum_row_actions', array (
-						'edit' => '<a href="' . add_query_arg( array( 'post' => $forum_id, 'action' => 'edit' ), admin_url( '/post.php' ) ) . '">' . __( 'Edit', 'bbpress' ) . '</a>',
-						'view' => '<a href="' . bbp_get_forum_permalink( $forum_id ) . '">' . __( 'View', 'bbpress' ) . '</a>'
-					) );
+					// Forum Title
+					if ( !$forum_title = bbp_get_forum_title( $forum_id ) )
+						$forum_title = __( 'No Forum', 'bbpress' );
 
-					// Output forum post row links
-					foreach ( $actions as $action => $link )
-						$formatted_actions[] = '<span class="' . $action . '">' . $link . '</span>';
+					// Output the title
+					echo $forum_title;
 
-					// Show forum actions
-					if ( !empty( $forum_id ) )
+					// Show actions if forum exists
+					if ( $forum_title != __( 'No Forum', 'bbpress' ) ) {
+
+						// Link information
+						$actions = apply_filters( 'reply_topic_forum_row_actions', array (
+							'edit' => '<a href="' . add_query_arg( array( 'post' => $forum_id, 'action' => 'edit' ), admin_url( '/post.php' ) ) . '">' . __( 'Edit', 'bbpress' ) . '</a>',
+							'view' => '<a href="' . bbp_get_forum_permalink( $forum_id ) . '">' . __( 'View', 'bbpress' ) . '</a>'
+						) );
+
+						// Output forum post row links
+						foreach ( $actions as $action => $link )
+							$formatted_actions[] = '<span class="' . $action . '">' . $link . '</span>';
+
+						// Show forum actions
 						echo '<div class="row-actions">' . implode( ' | ', $formatted_actions ) . '</div>';
+					}
 
 				// Reply has no forum
 				} else {
-					_e( '(No Forum)', 'bbpress' );
+					_e( 'No Forum', 'bbpress' );
 				}
 
 				break;

bbp-admin/bbp-topics.php
@@ -640,19 +640,29 @@ class BBP_Topics_Admin {
 
 				// Output forum name
 				if ( !empty( $forum_id ) ) {
-					bbp_forum_title( $forum_id );
+					
+					// Forum Title
+					if ( !$forum_title = bbp_get_forum_title( $forum_id ) )
+						$forum_title = __( 'No Forum', 'bbpress' );
 
-					// Link information
-					$actions = apply_filters( 'topic_forum_row_actions', array (
-						'edit' => '<a href="' . add_query_arg( array( 'post' => $forum_id, 'action' => 'edit' ), admin_url( '/post.php' ) ) . '">' . __( 'Edit', 'bbpress' ) . '</a>',
-						'view' => '<a href="' . bbp_get_forum_permalink( $forum_id ) . '">' . __( 'View', 'bbpress' ) . '</a>'
-					) );
+					// Output the title
+					echo $forum_title;
 
-					// Output forum post row links
-					foreach ( $actions as $action => $link )
-						$formatted_actions[] = '<span class="' . $action . '">' . $link . '</span>';
+					// Show actions if forum exists
+					if ( $forum_title != __( 'No Forum', 'bbpress' ) ) {
 
-					echo '<div class="row-actions">' . implode( ' | ', $formatted_actions ) . '</div>';
+						// Link information
+						$actions = apply_filters( 'topic_forum_row_actions', array (
+							'edit' => '<a href="' . add_query_arg( array( 'post' => $forum_id, 'action' => 'edit' ), admin_url( '/post.php' ) ) . '">' . __( 'Edit', 'bbpress' ) . '</a>',
+							'view' => '<a href="' . bbp_get_forum_permalink( $forum_id ) . '">' . __( 'View', 'bbpress' ) . '</a>'
+						) );
+
+						// Output forum post row links
+						foreach ( $actions as $action => $link )
+							$formatted_actions[] = '<span class="' . $action . '">' . $link . '</span>';
+
+						echo '<div class="row-actions">' . implode( ' | ', $formatted_actions ) . '</div>';
+					}
 				} else {
 					_e( '(No Forum)', 'bbpress' );
 				}

commit 2196d96d946585b392789822c723c6aeec3aeb96

Use correct _get_ function in reply post rows. Fig a bug introduced in r3260.
git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3272 9866e705-20ec-0310-96e7-cbb4277adcfb

bbp-admin/bbp-replies.php
@@ -570,7 +570,7 @@ class BBP_Replies_Admin {
 				if ( !empty( $topic_id ) ) {
 
 					// Topic Title
-					if ( !$topic_title = bbp_topic_title( $topic_id ) )
+					if ( !$topic_title = bbp_get_topic_title( $topic_id ) )
 						$topic_title = __( 'No Topic', 'bbpress' );
 
 					// Output the title