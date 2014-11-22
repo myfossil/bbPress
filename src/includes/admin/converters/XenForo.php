myfossil/bbPress

commit c2ebad5a5282f7e9b96fe373682c6b93720d73ab

Replies now use the reply ID for the the post name/slug via r5117. bbPress' importers should also reflect that change with the removal of 'Reply Title' (post_title), 'Reply Slug' (post_name) and `callback_reply_title` from all importers. Props netweb. Fixes #2585
git-svn-id: https://bbpress.svn.wordpress.org/trunk@5349 9866e705-20ec-0310-96e7-cbb4277adcfb

src/includes/admin/converters/XenForo.php
@@ -351,31 +351,6 @@ class XenForo extends BBP_Converter_Base {
 			'callback_method' => 'callback_userid'
 		);
 
-		// Reply title.
-		// Note: We join the 'thread' table because 'post' do not have titles.
-		$this->field_map[] = array(
-			'from_tablename'  => 'thread',
-			'from_fieldname'  => 'title',
-			'join_tablename'  => 'post',
-			'join_type'       => 'LEFT',
-			'join_expression' => 'USING (thread_id) WHERE thread.first_post_id != post.post_id',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_title',
-			'callback_method' => 'callback_reply_title'
-		);
-
-		// Reply slug (Clean name to avoid conflicts)
-		// Note: We join the 'thread' table because 'post' do not have titles.
-		$this->field_map[] = array(
-			'from_tablename'  => 'thread',
-			'from_fieldname'  => 'title',
-			'join_tablename'  => 'post',
-			'join_type'       => 'LEFT',
-			'join_expression' => 'USING (thread_id) WHERE thread.first_post_id != post.post_id',
-			'to_fieldname'    => 'post_name',
-			'callback_method' => 'callback_slug'
-		);
-
 		// Reply content.
 		$this->field_map[] = array(
 			'from_tablename'  => 'post',
@@ -710,16 +685,5 @@ class XenForo extends BBP_Converter_Base {
 	public function callback_topic_reply_count( $count = 1 ) {
 		$count = absint( (int) $count - 1 );
 		return $count;
-	}
-
-	/**
-	 * Set the reply title
-	 *
-	 * @param string $title XenForo topic title of this reply
-	 * @return string Prefixed topic title, or empty string
-	 */
-	public function callback_reply_title( $title = '' ) {
-		$title = !empty( $title ) ? __( 'Re: ', 'bbpress' ) . html_entity_decode( $title ) : '';
-		return $title;
 	}
 }

src/includes/admin/converters/AEF.php
@@ -361,32 +361,6 @@ class AEF extends BBP_Converter_Base {
 			'callback_method' => 'callback_userid'
 		);
 
-		// Reply title.
-		// Note: We join the 'topics' table because 'posts' table does not include reply title.
-		$this->field_map[] = array(
-			'from_tablename'  => 'topics',
-			'from_fieldname'  => 'topic',
-			'join_tablename'  => 'posts',
-			'join_type'       => 'INNER',
-			'join_expression' => 'ON topics.tid = posts.post_tid WHERE topics.first_post_id != posts.pid',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_title',
-			'callback_method' => 'callback_reply_title'
-		);
-
-		// Reply slug (Clean name to avoid conflicts)
-		// Note: We join the 'topics' table because 'posts' table does not include reply slug.
-		$this->field_map[] = array(
-			'from_tablename'  => 'topics',
-			'from_fieldname'  => 'topic',
-			'join_tablename'  => 'posts',
-			'join_type'       => 'INNER',
-			'join_expression' => 'ON topics.tid = posts.post_tid WHERE topics.first_post_id != posts.pid',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_name',
-			'callback_method' => 'callback_slug'
-		);
-
 		// Reply content.
 		$this->field_map[] = array(
 			'from_tablename'  => 'posts',
@@ -676,16 +650,5 @@ class AEF extends BBP_Converter_Base {
 	public function callback_topic_reply_count( $count = 1 ) {
 		$count = absint( (int) $count - 1 );
 		return $count;
-	}
-
-	/**
-	 * Set the reply title
-	 *
-	 * @param string $title AEF v1.0.9 topic title of this reply
-	 * @return string Prefixed topic title, or empty string
-	 */
-	public function callback_reply_title( $title = '' ) {
-		$title = !empty( $title ) ? __( 'Re: ', 'bbpress' ) . html_entity_decode( $title ) : '';
-		return $title;
 	}
 }

src/includes/admin/converters/Drupal7.php
@@ -358,23 +358,6 @@ class Drupal7 extends BBP_Converter_Base {
 			'callback_method' => 'callback_userid'
 		);
 
-		// Reply title.
-		$this->field_map[] = array(
-			'from_tablename' => 'comment',
-			'from_fieldname' => 'subject',
-			'to_type'        => 'reply',
-			'to_fieldname'   => 'post_title'
-		);
-
-		// Reply slug (Clean name to avoid conflicts)
-		$this->field_map[] = array(
-			'from_tablename'  => 'comment',
-			'from_fieldname'  => 'subject',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_name',
-			'callback_method' => 'callback_slug'
-		);
-
 		// Reply content.
 		// Note: We join the 'field_data_comment_body' table because 'comment' table does not include reply content.
 		$this->field_map[] = array(

src/includes/admin/converters/Example.php
@@ -224,6 +224,15 @@ class Example extends BBP_Converter_Base {
 			'callback_method' => 'callback_slug'
 		);
 
+		// Topic status (Open or Closed)
+		$this->field_map[] = array(
+			'from_tablename'  => 'topics_table',
+			'from_fieldname'  => 'the_topic_status',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_status',
+			'callback_method' => 'callback_topic_status'
+		);
+
 		// Topic parent forum id (If no parent, then 0)
 		$this->field_map[] = array(
 			'from_tablename'  => 'topics_table',
@@ -393,22 +402,9 @@ class Example extends BBP_Converter_Base {
 			'callback_method' => 'callback_userid'
 		);
 
-		// Reply title.
-		$this->field_map[] = array(
-			'from_tablename'  => 'replies_table',
-			'from_fieldname'  => 'the_reply_title',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_title'
-		);
-
-		// Reply slug (Clean name to avoid conflicts)
-		$this->field_map[] = array(
-			'from_tablename'  => 'replies_table',
-			'from_fieldname'  => 'the_reply_slug',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_name',
-			'callback_method' => 'callback_slug'
-		);
+		// Reply title and reply slugs
+		// Note: We don't actually want either a reply title or a reply slug as
+		//       we want single replies to use their ID as the permalink.
 
 		// Reply content.
 		$this->field_map[] = array(

src/includes/admin/converters/FluxBB.php
@@ -328,32 +328,6 @@ class FluxBB extends BBP_Converter_Base {
 			'callback_method' => 'callback_userid'
 		);
 
-		// Reply title.
-		// Note: We join the 'topics' table because 'posts' table does not include reply title.
-		$this->field_map[] = array(
-			'from_tablename'  => 'topics',
-			'from_fieldname'  => 'subject',
-			'join_tablename'  => 'posts',
-			'join_type'       => 'INNER',
-			'join_expression' => 'ON topics.id = posts.topic_id WHERE topics.first_post_id != posts.id',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_title',
-			'callback_method' => 'callback_reply_title'
-		);
-
-		// Reply slug (Clean name to avoid conflicts)
-		// Note: We join the 'topics' table because 'posts' table does not include slug title.
-		$this->field_map[] = array(
-			'from_tablename'  => 'topics',
-			'from_fieldname'  => 'subject',
-			'join_tablename'  => 'posts',
-			'join_type'       => 'INNER',
-			'join_expression' => 'ON topics.id = posts.topic_id WHERE topics.first_post_id != posts.id',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_name',
-			'callback_method' => 'callback_slug'
-		);
-
 		// Reply content.
 		$this->field_map[] = array(
 			'from_tablename'  => 'posts',
@@ -631,16 +605,5 @@ class FluxBB extends BBP_Converter_Base {
 	public function callback_topic_reply_count( $count = 1 ) {
 		$count = absint( (int) $count - 1 );
 		return $count;
-	}
-
-	/**
-	 * Set the reply title
-	 *
-	 * @param string $title FluxBB v1.5.3 topic title of this reply
-	 * @return string Prefixed topic title, or empty string
-	 */
-	public function callback_reply_title( $title = '' ) {
-		$title = !empty( $title ) ? __( 'Re: ', 'bbpress' ) . html_entity_decode( $title ) : '';
-		return $title;
 	}
 }

src/includes/admin/converters/Invision.php
@@ -324,19 +324,6 @@ class Invision extends BBP_Converter_Base {
 			'callback_method' => 'callback_userid'
 		);
 
-		// Reply title.
-		// Note: We join the topics table because post table does not include topic title.
-		$this->field_map[] = array(
-			'from_tablename'  => 'topics',
-			'from_fieldname'  => 'title',
-			'join_tablename'  => 'posts',
-			'join_type'       => 'INNER',
-			'join_expression' => 'ON (topics.tid = posts.topic_id) WHERE posts.new_topic = 0',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_title',
-			'callback_method' => 'callback_reply_title'
-		);
-
 		// Reply content.
 		$this->field_map[] = array(
 			'from_tablename'  => 'posts',
@@ -513,17 +500,6 @@ class Invision extends BBP_Converter_Base {
 	public function callback_topic_reply_count( $count = 1 ) {
 		$count = absint( (int) $count - 1 );
 		return $count;
-	}
-
-	/**
-	 * Set the reply title
-	 *
-	 * @param string $title Invision topic title of this reply
-	 * @return string Prefixed topic title, or empty string
-	 */
-	public function callback_reply_title( $title = '' ) {
-		$title = !empty( $title ) ? __( 'Re: ', 'bbpress' ) . html_entity_decode( $title ) : '';
-		return $title;
 	}
 
 	/**

src/includes/admin/converters/Kunena1.php
@@ -319,24 +319,6 @@ class Kunena1 extends BBP_Converter_Base {
         	'callback_method' => 'callback_userid'
 		);
 
-        // Reply title.
-        $this->field_map[] = array(
-        	'from_tablename' => 'kunena_messages',
-        	'from_fieldname' => 'subject',
-        	'to_type'        => 'reply',
-        	'to_fieldname'   => 'post_title',
-        	'callback_method' => 'callback_reply_title'
-		);
-
-        // Reply slug (Clean name to avoid conflicts)
-        $this->field_map[] = array(
-        	'from_tablename'  => 'kunena_messages',
-        	'from_fieldname'  => 'subject',
-        	'to_type'         => 'reply',
-        	'to_fieldname'    => 'post_name',
-        	'callback_method' => 'callback_slug'
-		);
-
         // Reply content.
         // Note: We join the 'kunena_messages_text' table because 'kunena_messages' table does not include reply content.
         $this->field_map[] = array(
@@ -531,16 +513,5 @@ class Kunena1 extends BBP_Converter_Base {
 				break;
 		}
 		return $status;
-	}
-
-	/**
-	 * Set the reply title
-	 *
-	 * @param string $title Kunena v1.x topic title of this reply
-	 * @return string Prefixed topic title, or empty string
-	 */
-	public function callback_reply_title( $title = '' ) {
-		$title = !empty( $title ) ? __( 'Re: ', 'bbpress' ) . html_entity_decode( $title ) : '';
-		return $title;
 	}
 }

src/includes/admin/converters/Kunena2.php
@@ -338,24 +338,6 @@ class Kunena2 extends BBP_Converter_Base {
 			'callback_method' => 'callback_userid'
 		);
 
-		// Reply title.
-		$this->field_map[] = array(
-			'from_tablename'  => 'kunena_messages',
-			'from_fieldname'  => 'subject',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_title',
-			'callback_method' => 'callback_reply_title'
-		);
-
-		// Reply slug (Clean name to avoid conflicts)
-		$this->field_map[] = array(
-			'from_tablename'  => 'kunena_messages',
-			'from_fieldname'  => 'subject',
-			'to_type'         => 'reply',
-			'to_fieldname'    => 'post_name',
-			'callback_method' => 'callback_slug'
-		);
-
 		// Reply content.
 		// Note: We join the 'kunena_messages_text' table because 'kunena_messages' table does not include reply content.
 		$this->field_map[] = array(
@@ -573,16 +555,5 @@ class Kunena2 extends BBP_Converter_Base {
 	public function callback_topic_reply_count( $count = 1 ) {
 		$count = absint( (int) $count - 1 );
 		return $count;
-	}
-
-	/**
-	 * Set the reply title
-	 *
-	 * @param string $title Kunena v2.x topic title of this reply
-	 * @return string Prefixed topic title, or empty string
-	 */
-	public function callback_reply_title( $title = '' ) {
-		$title = !empty( $title ) ? __( 'Re: ', 'bbpress' ) . html_entity_decode( $title ) : '';
-		return $title;
 	}
 }

commit 017f4e560f114e22a0009d5344dcb7fc323f946c

Xenforo 1.x importer: Fix regression introduced in r5349 where topics were imported a second time as part of the reply section import.
git-svn-id: https://bbpress.svn.wordpress.org/trunk@5568 9866e705-20ec-0310-96e7-cbb4277adcfb

src/includes/admin/converters/XenForo.php
@@ -324,6 +324,16 @@ class XenForo extends BBP_Converter_Base {
 			'to_fieldname'   => '_bbp_old_reply_id'
 		);
 
+		// Join the 'thread' table to exclude topics from being imported as replies
+		$this->field_map[] = array(
+			'from_tablename'  => 'thread',
+			'from_fieldname'  => 'thread_id',
+			'join_tablename'  => 'post',
+			'join_type'       => 'LEFT',
+			'join_expression' => 'USING (thread_id) WHERE thread.first_post_id != post.post_id',
+			'to_type'         => 'reply'
+		);
+
 		// Reply parent forum id (If no parent, then 0. Stored in postmeta)
 		$this->field_map[] = array(
 			'from_tablename'  => 'post',